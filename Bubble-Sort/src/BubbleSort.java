7a0b7ed2c80e72b5a4c7888384090b1c152b7470
Calum Boustead <calum.boustead@gmail.co.uk>
2017-12-21 18:51:46 +0000

    Bubble Sort
    Bubble sort working with hard coded array

---
+++ Bubble-Sort/src/BubbleSort.java
@@ -0,0 +1,47 @@
+public class BubbleSort {
+
+	
+
+	public static void main(String args[]) {
+		int size = 5;
+		int[] array = {5, 4, 1, 2, 3};
+		
+		
+		bubble(size, array);
+		
+		//print
+		for(int i=0; i<size; i++)
+		{
+			System.out.printf("%d ", array[i]);
+		}
+		
+
+		
+	}
+	
+	//bubble sort method
+	public static void bubble(int n, int[] ar)
+	{
+		for(int j=0; j<n-1;j++)
+		{
+			//bubble largest value to end
+			for(int k=0;k<n-1;k++)
+			{
+				//compare to ints
+				if(ar[k]>ar[k+1])
+				{
+					swap(k,k+1, ar);
+				}
+			}
+		}
+	}
+	
+	//method to swap elements in array
+	public static void swap(int a, int b, int[] ar)
+	{
+		int temp = ar[a];
+		ar[a] = ar[b];
+		ar[b] = temp;
+	}
+	
+}
