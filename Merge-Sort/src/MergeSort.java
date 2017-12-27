6f150b96951a5598fdfcb740386b3cd8fa2d749c
Calum Boustead <calum.boustead@gmail.co.uk>
2017-12-27 21:35:40 +0000

    Finished Merge Sort
    Added a test class to test the MergeSort class outside of its file

--- Merge-Sort/src/MergeSort.java
+++ Merge-Sort/src/MergeSort.java
@@ -2,10 +2,12 @@
 
 public class MergeSort {
 	
-	
-	private static void merge(int[] array)
+	MergeSort(){}
+	public static void merge(int[] array)
 	{
 		int length = array.length;
+		
+		//if array only has one value the return
 		if(length<2)
 		{
 			System.out.printf("%d ", array[0]);
@@ -28,15 +30,23 @@ public class MergeSort {
 			rightArray[j] = array[j + l];
 		}
 		
+		//recursively call until only one element in each array
 		merge(leftArray);
 		merge(rightArray);
-		
+
+		//merge the left and right arrays into one so they are in order
+		//The first merge will be on arrays with one element
+		// At the next level up, it'll be on arrays with two elements in order thanks to the first merge
+		// As the program comes out of the recursions it will continue to order correctly
 		doMerge(leftArray, rightArray, array);
 		
 		
 		
 	}
 	
+	
+	//do merge takes in the left and right array and puts them into another array
+	//the result array is taken in so that a new array does not have to be created, saving memory.
 	private static void doMerge(int[] leftA, int[] rightA, int[] resultA)
 	{
 		int ll = leftA.length;
@@ -45,8 +55,11 @@ public class MergeSort {
 		int i = 0;
 		int j = 0;
 		int k = 0;
+		//while there are element left in both array
 		while(i < ll && j < rl)
 		{
+			//check each of the smallest elements pointed to and put in result array
+			//then increment the counter so it is pointing at the next element/
 			if(leftA[i] < rightA[j])
 			{
 				resultA[k] = leftA[i];
@@ -58,6 +71,7 @@ public class MergeSort {
 			k++;
 		}
 		
+		//once one of the arrays are empty simply add the result of the elements in the other array to the result array
 		while(i<ll) {
 			resultA[k] = leftA[i];
 			i++;
@@ -70,22 +84,7 @@ public class MergeSort {
 		}
 	}
 	
-	public static void main(String[] args)
-	{
-		
-		int[] ar = {2,4,1,6,8,5,3,7};
-		merge(ar);
-		System.out.println();
-		
-		for(int i=0; i<8; i++)
-		{
-			System.out.printf("%d ", ar[i]);
-		}
-		
-		
-		
-	}
-
+	
 
 
 }
---
+++ Merge-Sort/src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+		
+		int[] ar = {2,4,1,6,8,5,3,7};
+		MergeSort test = new MergeSort();
+		test.merge(ar);
+		System.out.println();
+		
+		for(int i=0; i<8; i++)
+		{
+			System.out.printf("%d ", ar[i]);
+		}
+		
+		
+		
+	}
+
+}
